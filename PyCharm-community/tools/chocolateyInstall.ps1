38867746f186a1af888fbf69bfc03f46ece4fdc9
szaiser <stanzky@gmail.com>
2017-09-14 09:32:32 +0200

    pulled changes from user srtlg

--- PyCharm-community/tools/chocolateyInstall.ps1
+++ PyCharm-community/tools/chocolateyInstall.ps1
@@ -1,14 +1,11 @@
 ﻿$packageArgs = @{
   packageName   = 'PyCharm-community'
   fileType      = 'EXE'
-  url           = "https://download.jetbrains.com/python/pycharm-community-2016.3.3.exe"
-  url64bit      = "https://download.jetbrains.com/python/pycharm-community-2016.3.3.exe"
+  url           = "https://download.jetbrains.com/python/pycharm-community-2017.2.3.exe"
   silentArgs    = '/S'
   validExitCodes= @(0)
-  checksum      = '8f0bab6caceb922f2ac7904c722eb252040a929a26d4eeaad74405f07c085d32' 
+  checksum      = '0c05e26dd89bc45e4270ca22174e41148d23249d7fec770e0e3054b0cad60b67' 
   checksumType  = 'sha256'
-  checksum64    = '8f0bab6caceb922f2ac7904c722eb252040a929a26d4eeaad74405f07c085d32'
-  checksumType64= 'sha256'
 }
 
  
---
+++ PyCharm-community/tools/chocolateyUninstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "JetBrains PyCharm Community Edition $env:ChocolateyPackageVersion"
+  fileType      = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)
+  silentArgs   = '/S'           # NSIS
+  validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
