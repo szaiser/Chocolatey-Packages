ea4e8ac2568ed603e11c8108cc75191aedc575d3
Mirko Scholz <srtlg@users.noreply.github.com>
2017-11-02 15:02:29 +0100

    fixed package version
    since the package version is used to match the uninstall key, it
    must match exactly...

--- PyCharm-community/tools/chocolateyInstall.ps1
+++ PyCharm-community/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   packageName   = 'PyCharm-community'
   fileType      = 'EXE'
-  url           = "https://download.jetbrains.com/python/pycharm-community-2017.2.4.exe"
+  url           = "https://download.jetbrains.com/python/pycharm-community-$env:ChocolateyPackageVersion.exe"
   silentArgs    = '/S'
   validExitCodes= @(0)
   checksum      = 'd3312df221bf88420981f3405d85e23622807d487367f76e1b1e893e95837c70'
